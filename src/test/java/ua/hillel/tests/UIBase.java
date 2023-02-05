e504d84146aa19d23270854d9501482edbdb23df
NadiaKalnyk <kalnuk.2016.nadiya@gmail.com>
2023-02-05 17:50:24 +0200

    UI version 3 (added report)

--- src/test/java/ua/hillel/tests/UIBase.java
+++ src/test/java/ua/hillel/tests/UIBase.java
@@ -3,13 +3,16 @@ package ua.hillel.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import ua.hillel.UI.BasePage;
+import ua.hillel.tests.listeners.CustomExtentReportListener;
 
 import static com.codeborne.selenide.Selenide.open;
-
+@Listeners(CustomExtentReportListener.class)
 public class UIBase {
     static {
-        Configuration.reportsFolder = "test-result/reports/ExtentHtml.html";
+        System.setProperty("extent.reporter.html.start", "true");
+        System.setProperty("extent.reporter.html.out", "target/extentReport/ExtentHtml.html");
     }
     @BeforeTest
     public void setUp() {
